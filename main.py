afa0e901323ba9c6c44fde555bf63bf0c2fd3bf5
Ievgen Dilevskyi <e.dilevskiy@gmail.com>
2024-09-12 13:43:58 -0600

    Set up the screen

---
+++ main.py
@@ -0,0 +1,24 @@
+from turtle import Screen
+import time
+
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.tracer(2)
+screen.listen()
+screen.title("Pong")
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
