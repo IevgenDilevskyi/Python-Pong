b7a259d15c00f7dbbf6e1d25f18646e58fc7060b
Ievgen Dilevskyi <e.dilevskiy@gmail.com>
2024-09-12 16:19:43 -0600

    move ball and detect collision with the walls

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 import time
 from paddle import Paddle
+from ball import Ball
 
 
 screen = Screen()
@@ -10,6 +11,7 @@ screen.tracer(0)
 screen.title("Pong")
 player_one = Paddle((350, 0))
 player_two = Paddle((-350, 0))
+ball = Ball()
 
 
 screen.listen()
@@ -21,6 +23,12 @@ screen.onkey(player_two.down, "s")
 game_is_on = True
 while game_is_on:
     screen.update()
+    time.sleep(0.1)
+    ball.move()
+
+    # Detect collision with the wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
 
 
 
