385a934a49577eb3929ffe63262856ea3526c2f6
Ievgen Dilevskyi <e.dilevskiy@gmail.com>
2024-09-12 20:35:54 -0600

    Detect collision with the paddle

--- main.py
+++ main.py
@@ -22,20 +22,17 @@ screen.onkey(player_two.down, "s")
 
 game_is_on = True
 while game_is_on:
-    screen.update()
     time.sleep(0.1)
+    screen.update()
     ball.move()
 
     # Detect collision with the wall
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
 
-
-
-
-
-
-
+    # Detect collision with the paddle
+    if ball.distance(player_one) < 50 and ball.xcor() > 320 or ball.distance(player_two) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 
 
